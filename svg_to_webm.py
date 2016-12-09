ea1c52034b5780f704c583ab9c4bdb1379548232
Fergus Cooper <cooper@maths.ox.ac.uk>
2016-12-09 18:35:00 +0000

    Substantial changes

--- svg_to_webm.py
+++ svg_to_webm.py
@@ -1,17 +1,30 @@
 import os
 import re
 import subprocess
-import platform
 
 
-def svg_to_webm(sim_dir, webm_name='results.webm', webm_aspect_ratio=1.0):
+def svg_to_webm(sim_dir, webm_name='results.webm', webm_aspect_ratio=1.0, webm_duration=15.0):
 
-    # Validate data directory
+    # First, check that ffmpeg is installed
+    ffmpeg_version, ffmpeg_errors = subprocess.Popen(['ffmpeg', '-version'], stdout=subprocess.PIPE).communicate()
+    if not ffmpeg_version.startswith('ffmpeg version'):
+        raise Exception('svg_to_webm: ffmpeg does not seem to be installed: ' + ffmpeg_version)
+
+    # Validate input
     if not (os.path.isdir(sim_dir)):
-        raise Exception('svg_to_webm: Invalid simulation directory')
+        raise Exception('svg_to_webm: Invalid simulation directory: ' + sim_dir)
 
     if not webm_name.endswith('.webm'):
         webm_name += '.webm'
+
+    if webm_aspect_ratio < 1.0:
+        raise Exception('svg_to_webm: Invalid webm_aspect_ratio: ' + str(webm_aspect_ratio))
+
+    if webm_duration < 1.0:
+        raise Exception('svg_to_webm: Invalid webm_duration: ' + str(webm_duration))
+
+    # Set GZIP environment variable to give max compression in tar stages
+    os.environ['GZIP'] = '-9'
 
     # Check whether an svg archive exists.  If it does, extract it.  Then, list all svg files in the directory
     svg_archive_exists = 'svg_arch.tar.gz' in os.listdir(sim_dir)
@@ -28,26 +41,31 @@ def svg_to_webm(sim_dir, webm_name='results.webm', webm_aspect_ratio=1.0):
     # Use the first svg file to calculate information necessary to convert and crop the svg to png
     file_info = calculate_image_info(svg_file_path=os.path.join(sim_dir, svg_files[0]), aspect_ratio=webm_aspect_ratio)
 
+    print(file_info)
+
     # Convert each svg to png
     for idx, svg_file in enumerate(svg_files):
         svg_to_png(path_to_files=sim_dir, file_name=svg_file, file_info=file_info, index=idx)
         print('Converted svg ' + str(idx) + ' of ' + str(len(svg_files)))
 
-    png_files = list_files_of_type(path_name=sim_dir, extension='.png')
-    print(png_files)
+    # Tidy up: Remove svg files, adding them to an archive if they are not already archived
+    if svg_archive_exists:
+        subprocess.call(['rm'] + svg_files, cwd=sim_dir)
+    else:
+        subprocess.call(['tar', '-zcf', 'svg_arch.tar.gz', '--remove-files'] + svg_files, cwd=sim_dir)
 
     # Set how long you want the video to be (in seconds), and set the frame rate accordingly
-    video_duration = 15.0
-    frame_rate = str(float(len(png_files)) / video_duration)
+    png_files = list_files_of_type(path_name=sim_dir, extension='.png')
+    frame_rate = float(len(png_files)) / webm_duration
 
-    # Send the system command to run avconv/ffmpeg. Parameters:
-    #   -v 0                        Suppress console output so as not to clutter the terminal
-    #   -r frame_rate               Set the frame rate calculated above
-    #   -f image2                   Set the convert format (image sequence to video)
-    #   -i dir/results.%04d.png     Input expected as dir/results.####.png, the output from WriteAnimation above
-    #   -c:v h264                   Video codec to use is h264
-    #   -crf 0                      Set video quality: 0 best, 51 worst (https://trac.ffmpeg.org/wiki/Encode/H.264)
-    #   -y dir/movie.mp4            Output directory and name
+    # Send the subprocess call to run ffmpeg. Parameters:
+    #   -v 0               Suppress console output so as not to clutter the terminal
+    #   -r frame_rate      Set the frame rate calculated above
+    #   -f image2          Set the convert format (image sequence to video)
+    #   -i %04d.png        Input expected as dir/results.####.png, the output from WriteAnimation above
+    #   -c:v libvpx-vp9    Video codec to use is vp9
+    #   -lossless', '1'    Set video quality to lossless
+    #   -y name.webm       Output directory and name
     subprocess.call(['ffmpeg',
                      '-v', '0',
                      '-r', str(frame_rate),
@@ -57,32 +75,31 @@ def svg_to_webm(sim_dir, webm_name='results.webm', webm_aspect_ratio=1.0):
                      '-lossless', '1',
                      '-y', webm_name], cwd=sim_dir)
 
-    # # Raise exception if the mp4 file is not generated as expected
-    # if not (os.path.isfile(full_movie_path)):
-    #     raise Exception('pvd_to_mp4: mp4 not generated as expected')
-    #
-    # # Raise exception if the mp4 file file is created but is smaller than 1kb - ffmpeg sometimes
-    # # generates an empty file even if an error occurs
-    # if os.path.getsize(full_movie_path) < 1024:
-    #     raise Exception('pvd_to_mp4: mp4 not generated as expected')
+    # Raise exception if the webm file is not generated as expected
+    if not os.path.isfile(os.path.join(sim_dir, webm_name)):
+        raise Exception('svg_to_webm: webm not generated as expected')
 
+    # Raise exception if the webm file file is created but is smaller than 1kb - ffmpeg sometimes
+    # generates an empty file even if an error occurs
+    if os.path.getsize(os.path.join(sim_dir, webm_name)) < 1024:
+        raise Exception('svg_to_webm: webm not generated as expected')
 
-
-
-
-
-
-
-
-
-    # Remove svg files, adding them to an archive if they are not already archived
-    if svg_archive_exists:
-        subprocess.call(['rm'] + svg_files, cwd=sim_dir)
-    else:
-        subprocess.call(['tar', '-zcf', 'svg_arch.tar.gz', '--remove-files'] + svg_files, cwd=sim_dir)
-
-    # Delete all png files
+    # Tidy up: Delete all png files
     subprocess.call(['rm'] + png_files, cwd=sim_dir)
+
+    # Tidy up: compress any vtu and pvd files, if they exist
+    vtu_files = list_files_of_type(sim_dir, '.vtu') + list_files_of_type(sim_dir, '.pvd')
+    if len(vtu_files) > 0:
+        subprocess.call(['tar', '-zcf', 'vtu_arch.tar.gz', '--remove-files'] + vtu_files, cwd=sim_dir)
+
+    # Tidy up: compress any other results files, if they exist
+    res_files = []
+    for res_file in os.listdir(sim_dir):
+        if not (res_file.endswith('.webm') or res_file.endswith('.tar.gz') or res_file.startswith('.')):
+            res_files.append(res_file)
+    print(res_files)
+    if len(res_files) > 0:
+        subprocess.call(['tar', '-zcf', 'res_arch.tar.gz', '--remove-files'] + res_files, cwd=sim_dir)
 
 
 # Helper function for svg_to_webm: list all files with a particular extension in the simulation directory
@@ -113,23 +130,26 @@ def calculate_image_info(svg_file_path, aspect_ratio):
     if len(dimensions) != 2:
         raise Exception('svg_to_webm: Did not successfully extract svg dimensions from ' + svg_file_path)
 
-    file_info_dict = {'svg_width': dimensions[0],
+    file_info = {'svg_width': dimensions[0],
                       'svg_height': dimensions[1],
                       'png_width': int(dimensions[0]),
                       'png_height': int(dimensions[0] / aspect_ratio),
                       'png_y_offset': int(0.5 * (dimensions[1] - dimensions[1] / aspect_ratio))}
 
-    file_info_dict['crop_string'] = '{0}x{1}+0+{2}'.format(str(file_info_dict['png_width']),
-                                                           str(file_info_dict['png_height']),
-                                                           str(file_info_dict['png_y_offset']))
+    file_info['crop_string'] = '{0}:{1}:{2}:{3}'.format(str(0),
+                                                        str(file_info['png_y_offset']),
+                                                        str(file_info['png_width']),
+                                                        str(file_info['png_y_offset'] + file_info['png_height']))
 
-    return file_info_dict
+    return file_info
 
 
 def svg_to_png(path_to_files, file_name, file_info, index):
-    subprocess.call(['convert', os.path.join(path_to_files, file_name),
-                     '-crop', file_info['crop_string'],
-                     os.path.join(path_to_files, str(index).zfill(4) + '.png')])
+    FNULL = open(os.devnull, 'w')
+    subprocess.call(['inkscape', '-z',
+                     '-e', str(index).zfill(4) + '.png',
+                     '-a', file_info['crop_string'],
+                     file_name], cwd=path_to_files, stdout=FNULL)
 
 if __name__ == '__main__':
     svg_to_webm('/scratch/fcooper/chaste_test_output/TestShortMultiCellSimulation/results_from_time_0', 'test_movie', 16.0/9)
