14236e00efd5182ec9ed1d09d580b8b553318f00
Fergus Cooper <cooper@maths.ox.ac.uk>
2018-07-31 12:46:46 +0100

    A few changes to plotting script

--- heatmap.py
+++ heatmap.py
@@ -1,8 +1,26 @@
 import numpy as np
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
 grf = np.genfromtxt('/scratch/chaste_test_output/a_gaussian.csv', delimiter=',')
 
-im = plt.imshow(grf, interpolation='bilinear', origin='lower', extent=[0, 1, 0, 1])
+plt.figure(1, figsize=(24, 10), dpi=72)
+
+
+plt.subplot(1, 2, 1)
+plt.imshow(grf, interpolation='none', origin='lower', extent=[0, 1, 0, 1])
+
+
+# Generate N normally-distributed random numbers
+mu, sigma = 0, 1
+
+plt.subplot(1, 2, 2)
+plt.axis([-3*sigma, 3*sigma, 0, 0.5])
+plt.grid(True)
+n, bins, patches = plt.hist(grf.reshape(np.prod(grf.shape), 1), 50, normed=1, facecolor='green', alpha=0.75)
+
+# add a 'best fit' line
+y1 = mlab.normpdf(bins, mu, sigma)
+l1 = plt.plot(bins, y1, 'b--', linewidth=5)
 
 plt.show()
--- hist.py
+++ hist.py
@@ -2,36 +2,21 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
-N = 1000000`
+grf = np.genfromtxt('/scratch/chaste_test_output/a_hist.csv', delimiter=',')
 
-# Generate N normally-distributed random numbers
-mu, sigma = 0, np.pi / np.sqrt(2)
-x = mu + sigma*np.random.randn(N+1)
-
-# Get the piecewise diff of the numbers
-differences = np.diff(x)
-
-# Scale by the necessary amount
-scale_const = (x[N] - x[0]) / N
-scaled_draws = differences - scale_const
-
-print(x)
-print(scaled_draws)
-
-print(np.sum(scaled_draws))
+plt.figure(1, figsize=(16, 10), dpi=72)
 
 
-# the histogram of the data
-n, bins, patches = plt.hist(scaled_draws, 50, normed=1, facecolor='green', alpha=0.75)
+plt.subplot(1, 1, 1)
+
+mu, sigma = 0, 1
+
+plt.axis([-3*sigma, 3*sigma, 0, 0.5])
+plt.grid(True)
+n, bins, patches = plt.hist(grf, 50, normed=1, facecolor='green', alpha=0.75)
 
 # add a 'best fit' line
-y1 = mlab.normpdf(bins, mu, np.pi)
-l1 = plt.plot(bins, y1, 'r--', linewidth=1)
+y1 = mlab.normpdf(bins, mu, sigma)
+l1 = plt.plot(bins, y1, 'b--', linewidth=5)
 
-# y2 = mlab.normpdf(bins, mu, np.sqrt(2) * sigma)
-# l2 = plt.plot(bins, y2, 'b--', linewidth=1)
-
-plt.axis([-8, 8, 0, 0.2])
-plt.grid(True)
 plt.show()
-
