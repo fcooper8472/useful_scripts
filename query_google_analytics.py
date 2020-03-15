7af2c1ea57a3122cb90b92fa023b96641fea6b1a
Fergus Cooper <fcooper8472@gmail.com>
2020-03-15 21:00:04 +0000

    First draft of google analytics script

--- query_google_analytics.py
+++ query_google_analytics.py
@@ -1,10 +1,94 @@
 import json
-import random
 
-data = {
-    'unique_users': random.randint(11, 99),
-    'number_of_cats': random.randint(11, 99),
-}
+from apiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
 
-with open('/fs/website/people/fergus.cooper/google_analytics_data.json', 'w') as outfile:
-    json.dump(data, outfile)
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = '/users/ferper/Private/mythic-byway-271219-f2fc9fdcf89e.json'
+VIEW_ID = '195779635'
+
+
+def initialize_analyticsreporting():
+    """Initializes an Analytics Reporting API V4 service object.
+
+    Returns:
+      An authorized Analytics Reporting API V4 service object.
+    """
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        KEY_FILE_LOCATION, SCOPES)
+
+    # Build the service object.
+    analytics = build('analyticsreporting', 'v4', credentials=credentials)
+
+    return analytics
+
+
+def get_report(analytics):
+    """Queries the Analytics Reporting API V4.
+
+    Args:
+      analytics: An authorized Analytics Reporting API V4 service object.
+    Returns:
+      The Analytics Reporting API V4 response.
+    """
+    return analytics.reports().batchGet(
+        body={
+            'reportRequests': [
+                {
+                    'viewId': VIEW_ID,
+                    'dateRanges': [{'startDate': '30daysAgo', 'endDate': 'today'}],
+                    'metrics': [{'expression': 'ga:sessions'}],
+                    'dimensions': [{'name': 'ga:country'}]
+                }]
+        }
+    ).execute()
+
+
+def print_response(response):
+    """Parses and prints the Analytics Reporting API V4 response.
+
+    Args:
+      response: An Analytics Reporting API V4 response.
+    """
+
+    json_data = {}
+
+
+    for report in response.get('reports', []):
+
+        columnHeader = report.get('columnHeader', {})
+        dimensionHeaders = columnHeader.get('dimensions', [])
+        metricHeaders = columnHeader.get('metricHeader', {}).get('metricHeaderEntries', [])
+
+        for total in report.get('data', {}).get('totals', []):
+            json_data['total'] = total.get('values')[0]
+
+        for row in report.get('data', {}).get('rows', []):
+
+            json_country = ""
+            json_value = -1
+
+            dimensions = row.get('dimensions', [])
+            dateRangeValues = row.get('metrics', [])
+
+            for header, dimension in zip(dimensionHeaders, dimensions):
+                json_country = dimension
+
+            for i, values in enumerate(dateRangeValues):
+                for metricHeader, value in zip(metricHeaders, values.get('values')):
+                    json_value = value
+
+            json_data[json_country] = json_value
+
+    with open('/fs/website/people/fergus.cooper/google_analytics_data.json', 'w') as outfile:
+        json.dump(json_data, outfile)
+
+
+def main():
+    analytics = initialize_analyticsreporting()
+    response = get_report(analytics)
+    print_response(response)
+
+
+if __name__ == '__main__':
+    main()
